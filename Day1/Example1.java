6ef162c18a25a8d85404cb3c2559f0911df309e6
Sindhu61 <sindhubn5065@gmail.com>
2021-12-28 12:01:47 +0530

    Day1

---
+++ Day1/Example1.java
@@ -0,0 +1,17 @@
+     Example-1:
+-----------------------
+   Program:
+----------------------
+class Example{
+public static void main(String[] args){
+System.out.println("Hello World!");
+System.out.println("Hello Java!");
+}
+}
+
+   output:
+--------------------
+PS C:\517> javac Example1.java
+PS C:\517> java Example1      
+Hello World!
+Hello Java!
